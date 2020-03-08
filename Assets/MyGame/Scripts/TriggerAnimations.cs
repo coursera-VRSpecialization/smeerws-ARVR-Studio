63c404c3c93c60c7288c4a2c0eb19e9778402c5b
smeerws <hello@smeerws.at>
2020-03-08 14:22:04 +0100

    w1 ui interaction for stop and left is working partly, w2 sphere trigger event is not working yet

---
+++ Assets/MyGame/Scripts/TriggerAnimations.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class TriggerAnimations : MonoBehaviour
+{
+    private const string animParamRotateLeft = "isSetToRotateLeft";
+    private const string animParamRotateRight = "isSetToRotateRight";
+    private const string animParamRotateInactive = "isSetToInactive";
+
+    public Animator stateMachine;
+
+    void Start ()
+    {
+        SetAnimationParamInactive ();
+    }
+
+    void SetAnimationParamInactive ()
+    {
+        stateMachine.ResetTrigger (animParamRotateLeft);
+        stateMachine.ResetTrigger (animParamRotateRight);
+        stateMachine.SetTrigger (animParamRotateInactive);
+    }
+
+    void SetAnimatinParamRotationToLeft ()
+    {
+        stateMachine.SetTrigger (animParamRotateLeft);
+        stateMachine.ResetTrigger (animParamRotateRight);
+        stateMachine.ResetTrigger (animParamRotateInactive);
+    }
+
+    void SetAnimationParamRotationToRight ()
+    {
+        stateMachine.ResetTrigger (animParamRotateLeft);
+        stateMachine.SetTrigger (animParamRotateRight);
+        stateMachine.ResetTrigger (animParamRotateInactive);
+    }
+
+    public void RotateRight ()
+    {
+        SetAnimationParamRotationToRight ();
+        Debug.Log("Button pressed right");
+    }
+
+    public void RotateLeft()
+    {
+        SetAnimatinParamRotationToLeft ();
+        Debug.Log("Button pressed left");
+    }
+
+    public void RotateStop()
+    {
+        SetAnimationParamInactive ();
+        Debug.Log("Button pressed stop");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TriggerEnter" + other.name);
+    }
+
+
+}
